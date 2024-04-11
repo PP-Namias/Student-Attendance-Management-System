433a5171dc3dd09afa5b948ca7c84b35e7eaf23a
Jhon Keneth Namias <jkrbn99@gmail.com>
2024-04-11 23:33:47 +0800

    Login and Registration Page

---
+++ LoginPage/DbContexts/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using LoginPage.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace LoginPage.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Host=localhost; port=5432; Database=Database; User Id=postgres; Password=Namias99;";
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+
+        public void SaveUser(User user)
+        {
+            try
+            {
+                Users.Add(user);
+                SaveChanges();
+            }
+            catch (DbUpdateException ex)
+            {
+                // Handle any exceptions or logging here
+                Console.WriteLine($"Error saving user: {ex.Message}");
+                throw; // Rethrow the exception to propagate it further if needed
+            }
+        }
+    }
+}
